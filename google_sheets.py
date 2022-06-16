a6ac9b4e3187f0133df23b4dfea0f9144874a934
Vladimir Irkhin <ivb1989@yandex.ru>
2022-06-16 22:36:04 +0300

    Add idea delete rows in db if him dont search in google sheet.

--- google_sheets.py
+++ google_sheets.py
@@ -13,7 +13,8 @@ from sqlalchemy import Column, Integer, Date, select
 from sqlalchemy.orm import sessionmaker
 from psycopg2.errors import UniqueViolation
 from sqlalchemy.exc import IntegrityError
-from sqlalchemy.orm.attributes import InstrumentedAttribute
+from sqlalchemy import func
+
 
 import requests
 from xml.etree import ElementTree
@@ -46,22 +47,6 @@ class DataBaseSheet(Base):
     price_rub = Column(Integer, nullable=False)
     date = Column(Date, nullable=False)
 
-    # def update(self):
-    #     s = Session()
-    #     mapped_values = {}
-    #     for item in DataBaseSheet.__dict__.items():
-    #         field_name = item[0]
-    #         field_type = item[1]
-    #         is_column = isinstance(field_type, InstrumentedAttribute)
-    #         if is_column:
-    #             mapped_values[field_name] = getattr(self, field_name)
-    #
-    #     s.query(DataBaseSheet).filter(DataBaseSheet.id == self.id).update(mapped_values)
-    #     s.commit()
-
-    # def add(self):
-    #     pass
-
     def __str__(self):
         return f"{self.order_number}"
 
@@ -91,7 +76,7 @@ class GoogleSheetDate:
             range='A:D',
             majorDimension='ROWS'
         ).execute()
-        return values['values'][1:3]
+        return values['values'][1:]
 
     def get_revisions_file(self):
         # Получение последней версии ревизии файла id
@@ -106,91 +91,63 @@ class GoogleSheetDate:
 
 
 def main():
-    # Создаем объект сессии из фабрики Session
-    # session = Session()
-    # Создаем объект GoogleSheetDate для получения доступа к гугл документу и чтению данных
-    # breakpoint()
     gs = GoogleSheetDate(credentials_file, spreadsheet_id)
     google_file = gs.read_file()
-    for line in google_file:
-        id_number, order_number, price, date = line
-        row = DataBaseSheet(id_number=id_number, order_number=order_number,
-                            price=price, price_rub=current_exchange_usd_to_rub(price),
-                            date=date)
 
-        # проверяем существует ли order_number, то есть такой элемент уже в базе
-        if session.query(DataBaseSheet).filter_by(order_number=order_number).first() is not None:
-            print(order_number)
-            print('Есть такой элемент')
+    # анализ какие есть номера заказов в базе, а каких нет в гугл доке
+    # если номеров заказов нет в гугл доке, то в дальнейшем удаляем из базы их
+    set_order_db = set([elem[0] for elem in session.query(DataBaseSheet.order_number).all()])
+    set_order_google = set([int(elem[1]) for elem in google_file])
+    print(set_order_db)
+    print(set_order_google)
+    print(set_order_db.difference(set_order_google))
 
-            session.query(DataBaseSheet).filter(DataBaseSheet.order_number == order_number).update(
-                {
-                    "id_number": id_number,
-                    "order_number": order_number,
-                    "price": price,
-                    "price_rub": current_exchange_usd_to_rub(price),
-                    "date": date
-                },
-                synchronize_session=False
-            )
-            session.commit()
-            # # breakpoint()
-            # elem = session.query(DataBaseSheet).filter_by(order_number == order_number).first()
-            # if elem.id_number != id_number or elem.price != price or elem.date != date:
-            #     elem.update(
-            #         {
-            #             "id_number": id_number,
-            #             "order_number": order_number,
-            #             "price": price,
-            #             "price_rub": current_exchange_usd_to_rub(price),
-            #             "date": date
-            #         },
-            #         synchronize_session='evaluate'
-            #     )
-            #     # update_row(elem, line)
-            #     session.commit()
-            # if elem.id_number != id_number or elem.price != price or elem.date != date:
-            #     elem.id_number = id_number
-            #     elem.price = price
-            #     elem.date = date
-            #     elem.price_rub = current_exchange_usd_to_rub(price)
-            #     session.commit()
+    # for line in google_file:
+    #     id_number, order_number, price, date = line
+    #     row = DataBaseSheet(id_number=id_number, order_number=order_number,
+    #                         price=price, price_rub=current_exchange_usd_to_rub(price),
+    #                         date=date)
+    #
+    #     # проверяем существует ли order_number, то есть такой элемент уже в базе
+    #     if session.query(DataBaseSheet).filter_by(order_number=order_number).first() is not None:
+    #         print(order_number)
+    #         print('Есть такой элемент')
+    #
+    #         session.query(DataBaseSheet).filter(DataBaseSheet.order_number == order_number).update(
+    #             {
+    #                 "id_number": id_number,
+    #                 "order_number": order_number,
+    #                 "price": price,
+    #                 "price_rub": current_exchange_usd_to_rub(price),
+    #                 "date": date
+    #             },
+    #             synchronize_session=False
+    #         )
+    #         session.commit()
+    #
+    #     else:
+    #         # Если нет, то создаем новую запись.
+    #         # row = DataBaseSheet(id_number=id_number, order_number=order_number,
+    #         #                     price=price, price_rub=current_exchange_usd_to_rub(price),
+    #         #                     date=date)
+    #         # ловим возможную ошибку, например, такая запись уже есть.
+    #         # если ошибку нашли, то перехватываем и делаем rollback
+    #         try:
+    #             # Добавляем запись
+    #             session.add(row)
+    #
+    #             # добавляем данные в таблицу
+    #             session.commit()
+    #         except (UniqueViolation, IntegrityError) as e:
+    #             print('A duplicate record already exists')
+    #             session.rollback()
+    #         finally:
+    #             session.close()
+    #
+    #         # А теперь попробуем вывести все посты , которые есть в нашей таблице
+    #         # for row in session.query(DataBaseSheet):
+    #         #     print(row)
 
-            print('Есть такой элемент')
-            # делаем обработку, есть ли изменения в данных и если они есть, то мы меняем данные
-            # если нет, то переходим к другому элементы из гугл таблицы
-        else:
-            # Если нет, то создаем новую запись.
-            # row = DataBaseSheet(id_number=id_number, order_number=order_number,
-            #                     price=price, price_rub=current_exchange_usd_to_rub(price),
-            #                     date=date)
-            # ловим возможную ошибку, например, такая запись уже есть.
-            # если ошибку нашли, то перехватываем и делаем rollback
-            try:
-                # Добавляем запись
-                session.add(row)
-
-                # добавляем данные в таблицу
-                session.commit()
-            except (UniqueViolation, IntegrityError) as e:
-                print('A duplicate record already exists')
-                session.rollback()
-            finally:
-                session.close()
-
-            # А теперь попробуем вывести все посты , которые есть в нашей таблице
-            # for row in session.query(DataBaseSheet):
-            #     print(row)
-
-
-# def update_row(row, line):
-#     id_number, order_number, price, date = line
-#     if row.id_number != row or row.price != price or row.date != date:
-#         row.id_number = id_number
-#         row.price = price
-#         row.date = date
-#         row.price_rub = current_exchange_usd_to_rub(price)
-#         # session.commit()
 
 def current_exchange_usd_to_rub(cost_usd):
     """
